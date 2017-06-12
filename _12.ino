0263a5aafa8329518e4da5cdc61392ae614d2c07
yoyoturtle <yoyoturtle@users.noreply.github.com>
2017-06-12 20:01:30 +0800

    Add files via upload

---
+++ _12.ino
@@ -0,0 +1,26 @@
+//#include <doxygen.h>
+#include <ESP8266.h>
+#include <SoftwareSerial.h>
+
+
+SoftwareSerial mySerial(10,11);
+//ESP8266 wifi(mySerial);
+
+void setup() {
+  mySerial.begin(115200); //RX1 TX1，即 Pin18 及 Pin19
+  Serial.begin(9600);
+  Serial.println("Start");
+  //pinMode(13,OUTPUT);
+}
+void loop()
+{
+  if (mySerial.available()) {
+    Serial.write(mySerial.read());
+  }
+ 
+  if (Serial.available()) {
+    char chars = Serial.read();
+    mySerial.write(chars);
+    //digitalWrite(13,HIGH);
+  }
+}
